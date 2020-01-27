376345e4a5a9b9a4ff1faabe58c959e54945001d
financeiro03 <financeiro03@FIN03.rsatacadao.local>
2020-01-27 15:17:52 -0300

    criação da form principal

---
+++ CrudStudant/src/br/com/java/main/LoginFormMain.java
@@ -0,0 +1,18 @@
+package br.com.java.main;
+
+import javax.swing.JFrame;
+
+public class LoginFormMain extends JFrame {
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		
+		StudentForm fr = new StudentForm();
+		fr.setVisible(true);
+	}
+
+}
