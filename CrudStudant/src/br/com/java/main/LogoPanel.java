92346b69908d41124a06114d929f2795c25bb7c0
financeiro03 <financeiro03@FIN03.rsatacadao.local>
2020-01-27 15:25:47 -0300

    Criação da classe logoPanel

---
+++ CrudStudant/src/br/com/java/main/LogoPanel.java
@@ -0,0 +1,28 @@
+package br.com.java.main;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+//IMP: project>new> Source Folder> resources folder for images
+//Eclipse does not create resource folder in class path it paste directly
+//in main path, so dont use resources folder, give name directly liek "/nepal.png"
+public class LogoPanel extends JPanel {
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		try {
+			super.paintComponent(g);
+
+			BufferedImage bImg = ImageIO.read(LogoPanel.class.getResource("/student_login.png"));
+			g.drawImage(bImg, 0, 0, 195, 100, null);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
